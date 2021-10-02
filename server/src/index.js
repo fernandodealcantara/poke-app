f7891d404d03090df5b61eaa520619c4adebab28
Fernando <45339608+fernandodealcantara@users.noreply.github.com>
2021-10-02 20:27:36 -0300

    Update server index.js

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express();
 
-app.use(cors({origin: 'http://localhost:5000'}));
+app.use(cors({origin: 'https://poke-app-nine.vercel.app'}));
 
 app.get('/', (req, res) => {
   res.send('Hello to Poke Guess!')
