cbf8bf0adfcde8453d70dfdeeee426e0aca093f2
Rodrigo Pereira Couto <rodrigopereiracouto@hotmail.com>
2021-10-23 20:42:12 -0300

    implements infinity scroll

--- client/src/api/pokedex.js
+++ client/src/api/pokedex.js
@@ -1,45 +1,68 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'https://pokedex20201.herokuapp.com'});
+const API = axios.create({ baseURL: "https://pokedex20201.herokuapp.com" });
 
 export const getPokemons = async (page) => {
   try {
     const { data } = await API.get(`/pokemons?page=${page}`);
-    return data
-  } catch(error)  {
-    return {error: 'Error trying to get pokemons.'}
+    return data;
+  } catch (error) {
+    return { error: "Error trying to get pokemons." };
   }
-}
+};
 
 export const getUser = async (username) => {
   try {
     const { data } = await API.get(`/users/${username}`);
-    if (data)
-      return data
-  } catch(error) {
+    if (data) return data;
+  } catch (error) {
     try {
-      const { data } = await API.post('/users', { username });
-      return {user: data, pokemons: []};
-    } catch(error) {
-      return {error: 'Error trying to get/create user.'}
+      const { data } = await API.post("/users", { username });
+      return { user: data, pokemons: [] };
+    } catch (error) {
+      return { error: "Error trying to get/create user." };
     }
   }
-}
+};
 
 export const postUserFavPokemons = async (username, pokemon) => {
   try {
     const { data } = await API.post(`/users/${username}/starred/${pokemon}`);
     return { data };
-  } catch(error) {
-    return {error: 'Error trying to post favorite pokemon.'}
+  } catch (error) {
+    return { error: "Error trying to post favorite pokemon." };
   }
-}
+};
 
 export const delFromUserFavPokemons = async (username, pokemon) => {
   try {
     const { data } = await API.delete(`/users/${username}/starred/${pokemon}`);
     return { data };
-  } catch(error) {
-    return {error: 'Error trying to delete favorite pokemon.'}
+  } catch (error) {
+    return { error: "Error trying to delete favorite pokemon." };
   }
+};
+
+export const getPokemon = async (pokemonName) => {
+  try {
+    const { data } = await API.get(`/pokemons/${pokemonName}`);
+    return { data }
+  } catch (error) {
+    return { error: "Error trying to get pokemon" };
+  }
+};
+
+function range(start, end) {
+  return Array(end - start + 1)
+    .fill()
+    .map((_, idx) => API.get(`/pokemons?page=${start + idx}`));
 }
+export const getAllPokemonsFromAllPages = async (pagI, pagF) => {
+  try {
+    const pages = await Promise.all(range(pagI, pagF));
+    const pokemons = pages.map((page) => page.data.data).flat();
+    return { data: pokemons };
+  } catch (error) {
+    return { error: "Error trying to get pokemons." };
+  }
+};
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,79 +1,153 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './styles.css';
-import Pokemon  from '../Card/Pokemon.js';
-import { getPokemons, postUserFavPokemons, delFromUserFavPokemons } from '../../api/pokedex.js';
+import React, { useEffect, useState, useRef } from "react";
+import "./styles.css";
+import Pokemon from "../Card/Pokemon.js";
+import {
+  getAllPokemonsFromAllPages,
+  postUserFavPokemons,
+  delFromUserFavPokemons,
+} from "../../api/pokedex.js";
+import { AutoSizer, List } from "react-virtualized";
+import "react-virtualized/styles.css"; // only needs to be imported once
 
 const Home = ({ player, setPlayer }) => {
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
   const [allPokemons, setAllPokemons] = useState([]);
   const [userPokemons, setUserPokemons] = useState([]);
-  const [pageInfo, setPageInfo] = useState(undefined);
-  const [page, setPage] = useState(1);
-  const [display, setDisplay] = useState(false)
-  const favPokes = useRef(null)
+  const [display, setDisplay] = useState(false);
+  const favPokes = useRef(null);
+  const [search, setSearch] = useState("");
+  const ITEMS_COUNT = filteredPokemons.length;
+  const ITEM_SIZE = 200;
 
-  const goBack = () => { if (pageInfo?.prev_page) setPage(pageInfo?.prev_page)}
-  const goNext = () => { if (pageInfo?.next_page) setPage(pageInfo?.next_page)}
+  const onWheel = (e) => (favPokes.current.scrollLeft += e.deltaY);
 
-  const onWheel = (e) => favPokes.current.scrollLeft += e.deltaY
-
-  const fetchPokemons = async () => {
-    const {data, size, next_page, prev_page, error} = await getPokemons(page);
+  const fetchAllPokemons = async () => {
+    const { data, error } = await getAllPokemonsFromAllPages(1, 33);
     if (error) return;
-    const userPokemonsIds = userPokemons.map((pokemon) => pokemon.id)
-    setAllPokemons(data.filter((pokemon) => !userPokemonsIds.includes(pokemon.id)));
-    setPageInfo({size, next_page, prev_page});
-  }
+    setAllPokemons(data);
+  };
 
   const handleAddFavoritePokemons = async (pokemon) => {
     if (player) {
-      const {data, error} = await postUserFavPokemons(player.user.username, pokemon.name)
-      
+      const { data, error } = await postUserFavPokemons(
+        player.user.username,
+        pokemon.name
+      );
+
       if (error) return;
 
-      setPlayer(data)
+      setPlayer(data);
     }
-  }
+  };
 
   const handleRemFavoritePokemons = async (pokemon) => {
     if (player) {
-      const {data, error} = await delFromUserFavPokemons(player.user.username, pokemon.name)
+      const { data, error } = await delFromUserFavPokemons(
+        player.user.username,
+        pokemon.name
+      );
 
       if (error) return;
 
-      setPlayer(data)
+      setPlayer(data);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchPokemons()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [userPokemons, page])
+    const userPokemonsIds = userPokemons.map((pokemon) => pokemon.id);
+    const TodosPokemons = allPokemons
+      .filter((pokemon) => pokemon.name.includes(search.toLowerCase()))
+      .filter((pokemon) => !userPokemonsIds.includes(pokemon.id));
+    setFilteredPokemons(TodosPokemons);
+  }, [allPokemons, search, userPokemons]);
+
+  useEffect(() => {
+    fetchAllPokemons();
+  }, []);
 
   useEffect(() => {
     if (player) {
-      setUserPokemons(player.pokemons)
+      setUserPokemons(player.pokemons);
     } else {
-      setUserPokemons([])
+      setUserPokemons([]);
     }
-  }, [player])
+  }, [player]);
 
   return (
     <div className="Container">
-      <button style={{ display: player ? "block" : "none"}} className="button" onClick={() => setDisplay(!display)}>
+      <button
+        style={{ display: player ? "block" : "none" }}
+        className="button"
+        onClick={() => setDisplay(!display)}
+      >
         Meus Pokemons
       </button>
-      <div ref={favPokes} onWheel={onWheel} style={{ display: display && player ? "flex" : "none" }} className="favoritePokemons">
-          {userPokemons.map((pokemon) => <Pokemon handlePokemon={handleRemFavoritePokemons} useDelete key={pokemon.id} pokemon={pokemon}/>)}
+      <div
+        ref={favPokes}
+        onWheel={onWheel}
+        style={{ display: display && player ? "flex" : "none" }}
+        className="favoritePokemons"
+      >
+        {userPokemons.map((pokemon) => (
+          <Pokemon
+            handlePokemon={handleRemFavoritePokemons}
+            useDelete
+            key={pokemon.id}
+            pokemon={pokemon}
+          />
+        ))}
+        ,
       </div>
       <div className="allPokemons">
-        {allPokemons.map((pokemon) => <Pokemon handlePokemon={handleAddFavoritePokemons} pokemon={pokemon} key={pokemon.id} />)}
+        <AutoSizer>
+          {({ height, width }) => {
+            const itemsPerRow = Math.floor(width / ITEM_SIZE);
+            const rowCount = Math.ceil(ITEMS_COUNT / itemsPerRow);
+
+            return (
+              <List
+                className="List"
+                width={width}
+                height={height}
+                rowCount={rowCount}
+                rowHeight={ITEM_SIZE}
+                rowRenderer={({ index, key, style }) => {
+                  const items = [];
+                  const fromIndex = index * itemsPerRow;
+                  const toIndex = Math.min(
+                    fromIndex + itemsPerRow,
+                    ITEMS_COUNT
+                  );
+                  for (let i = fromIndex; i < toIndex; i++) {
+                    items.push(
+                      <Pokemon
+                        handlePokemon={handleAddFavoritePokemons}
+                        pokemon={filteredPokemons[i]}
+                        key={i}
+                      />
+                    );
+                  }
+                  return (
+                    <div className="Row" key={key} style={style}>
+                      {items}
+                    </div>
+                  );
+                }}
+              />
+            );
+          }}
+        </AutoSizer>
       </div>
-      <div className='changePages'>
-        <button className='button pageButtons' onClick={goBack}>Voltar</button>
-        <button className='button pageButtons' onClick={goNext}>Avançar</button>
+      <div className="searchBar">
+        <input
+          type="text"
+          className="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
